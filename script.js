3416e45d5649819b982d3dac55e640b0834ef201
JMD1905 <369mehmet1905@gmail.com>
2026-04-17 12:55:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const maclar = [
-{ takim1: "Galatasaray", takim2: "Fenerbahçe", skor: "2-1" },
-{ takim1: "Beşiktaş", takim2: "Trabzonspor", skor: "1-1" }
+    { lig: "Süper Lig", takim1: "Galatasaray", takim2: "Fenerbahçe", skor: "2 - 1", durum: "Canlı" },
+    { lig: "Süper Lig", takim1: "Beşiktaş", takim2: "Trabzonspor", skor: "1 - 1", durum: "Devre Arası" },
+    { lig: "Premier Lig", takim1: "Arsenal", takim2: "Chelsea", skor: "3 - 2", durum: "Canlı" }
 ];
 
 const alan = document.getElementById("maclar");
 
-maclar.forEach(mac => {
-const div = document.createElement("div");
-div.className = "mac";
-div.innerHTML = `${mac.takim1} ${mac.skor} ${mac.takim2}`;
-alan.appendChild(div);
-});
+function render() {
+    alan.innerHTML = "";
+
+    maclar.forEach(mac => {
+        const div = document.createElement("div");
+        div.className = "mac";
+
+        div.innerHTML = `
+            <h3>${mac.lig}</h3>
+            <p><b>${mac.takim1}</b> ${mac.skor} <b>${mac.takim2}</b></p>
+            <small>${mac.durum}</small>
+        `;
+
+        alan.appendChild(div);
+    });
+}
+
+render();
